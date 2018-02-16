a45b016eb55d1f5b48163738e53506f511289c61
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-02-15 19:30:50 -0700

    added commander support

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const app = require('commander');
+
+app
+ .arguments('<file>')
+ .option('-u, --username <username>', 'The user to authenticate as')
+ .option('-p, --password <password>', 'The user\'s password')
+ .action(function(file) {
+   console.log('user: %s pass: %s file: %s',
+       app.username, app.password, file);
+ })
+ .parse(process.argv);
