ec4601dee4e5e4ebdbcccc5206e12d85fcf69234
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-02-15 22:07:45 -0700

    added command for seeing, fixed elint rule for console.logs

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 const app = require('commander');
+const fs = require('fs');
+const path = require('path');
 
+/**
+ * Need to create a seeder file template
+ *
+ */
 app
-  .arguments('<file>')
-  .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The user\'s password')
-  .action((file) => {
-    console.log('user: %s pass: %s file: %s', app.username, app.password, file);
-  })
-  .parse(process.argv);
+  .command('run')
+  .option('-p, --path <path>', 'path to seeder directory')
+  .action((ops) => {
+    const dirPath = ops.path ? ops.path : 'src';
+    fs.readdir(path.resolve(__dirname, `./${dirPath}`), (err, files) => {
+      files.forEach((file, index) => {
+        console.log(`seending ${(index + 1)} of ${files.length}`);
+        const seeder = require(path.resolve(__dirname, `${dirPath}/${file}`));
+        seeder.up();
+      });
+
+      console.log('seed complete 🌱');
+    });
+  });
+
+app.parse(process.argv);
