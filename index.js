da62796ae3f58a0f65a2014d8197c717c23b0071
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-02-15 19:34:35 -0700

    added eslint support

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app
  .arguments('<file>')
  .option('-u, --username <username>', 'The user to authenticate as')
  .option('-p, --password <password>', 'The user\'s password')
- .action(function(file) {
+ .action((file) => {
    console.log('user: %s pass: %s file: %s',
        app.username, app.password, file);
  })
