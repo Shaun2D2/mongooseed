4afe1ff581f01b9ac68d689acd5b7054117d9b4b
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-02-15 19:39:09 -0700

    fixed linting rules

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const app = require('commander');
 
 app
- .arguments('<file>')
- .option('-u, --username <username>', 'The user to authenticate as')
- .option('-p, --password <password>', 'The user\'s password')
- .action((file) => {
-   console.log('user: %s pass: %s file: %s',
-       app.username, app.password, file);
- })
- .parse(process.argv);
+  .arguments('<file>')
+  .option('-u, --username <username>', 'The user to authenticate as')
+  .option('-p, --password <password>', 'The user\'s password')
+  .action((file) => {
+    console.log('user: %s pass: %s file: %s', app.username, app.password, file);
+  })
+  .parse(process.argv);
